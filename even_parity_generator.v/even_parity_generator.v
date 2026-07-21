c952095b5820385ac69febbf55114d2cad1e6a79
megala <mmegalaece@gmail.com>
2026-07-21 10:01:25 +0000

    even_parity_generator

---
+++ even_parity_generator.v/even_parity_generator.v
@@ -0,0 +1,6 @@
+module even_parity_generator(
+    input [3:0] data,
+    output parity
+);
+assign parity = data[3] ^ data[2] ^ data[1] ^ data[0];
+endmodule
---
+++ even_parity_generator.v/even_parity_generator_tb.v
@@ -0,0 +1,17 @@
+module even_parity_generator_tb;
+reg [3:0] data;
+wire parity;
+even_parity_generator uut(
+    .data(data),
+    .parity(parity)
+);
+initial begin
+$monitor("Time=%0t data=%b parity=%b", $time, data, parity);
+data = 4'b0000;#10;
+data = 4'b0001;#10;
+data = 4'b1010;#10;
+data = 4'b1111;#10;
+data = 4'b1001;#10;
+$finish;
+end
+endmodule
